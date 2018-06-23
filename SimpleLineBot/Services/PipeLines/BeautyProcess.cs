25825c45d131445bca8e4b0ef4eaaf274d556c25
XuPeiYao <st89382000@gmail.com>
2018-06-24 02:11:07 +0800

    表特版關鍵字搜尋與推文過濾

--- SimpleLineBot/Services/PipeLines/BeautyProcess.cs
+++ SimpleLineBot/Services/PipeLines/BeautyProcess.cs
@@ -20,23 +20,50 @@ namespace SimpleLineBot.Services.PipeLines {
 
             if (e.Message?.Text?.IndexOf("妹子") != 0) return false;
 
+            var command = e.Message?.Text.Split(' ');
+
             HttpClient client = new HttpClient();
             Uri image = null;
-            while (image == null) {
-                var url = await GetImageUrl();
-                if (url != null) {
-                    var response = await client.GetAsync(url);
-                    if (response.IsSuccessStatusCode) {
-                        image = new Uri(url);
+            int tryLimit = 10;
+
+            if (command.Length == 1) {
+                while (image == null && tryLimit > 0) {
+                    var url = await GetImageUrl();
+                    if (url != null) {
+                        var response = await client.GetAsync(url);
+                        if (response.IsSuccessStatusCode) {
+                            image = new Uri(url);
+                        }
                     }
+                    tryLimit--;
+                }
+            } else {
+                while (image == null && tryLimit > 0) {
+                    var url = await GetImageUrlByKeyword(command[1]);
+                    if (url != null) {
+                        var response = await client.GetAsync(url);
+                        if (response.IsSuccessStatusCode) {
+                            image = new Uri(url);
+                        }
+                    }
+                    tryLimit--;
                 }
             }
 
-            await Bot.Reply(e.ReplyToken, new ImageMessage() {
-                Url = image,
-                PreviewUrl = image
-            });
-
+            if (image == null) {
+                await Bot.Reply(e.ReplyToken,
+                    new TextMessage() {
+                        Text = "對不起!!現在找不到你要的妹子"
+                    }, new ImageMessage() {
+                        Url = new Uri("https://img.moegirl.org/common/c/c4/Owabi.jpg"),
+                        PreviewUrl = new Uri("https://img.moegirl.org/common/c/c4/Owabi.jpg")
+                    });
+            } else {
+                await Bot.Reply(e.ReplyToken, new ImageMessage() {
+                    Url = image,
+                    PreviewUrl = image
+                });
+            }
             return true;
         }
 
@@ -85,6 +112,11 @@ namespace SimpleLineBot.Services.PipeLines {
                     await client.GetStringAsync("https://www.ptt.cc" + posts[rand.Next(0, posts.Length)])
                 );
 
+                var pushs = tempPage.DocumentNode.SelectNodes("//div[contains(@class, 'push')]");
+                foreach (var push in pushs) {
+                    push.Remove();
+                }
+
                 Regex imgur = new Regex(@"https?:\/\/i\.imgur\.com\/.*");
 
                 var urls = tempPage.DocumentNode
@@ -104,5 +136,76 @@ namespace SimpleLineBot.Services.PipeLines {
                 return null;
             }
         }
+
+        public async Task<string> GetImageUrlByKeyword(string keyword) {
+            HttpClient client = new HttpClient();
+
+            var mainPage = new HtmlDocument();
+
+            mainPage.LoadHtml(
+                await client.GetStringAsync("https://www.ptt.cc/bbs/Beauty/search?page=1&q=" + Uri.EscapeDataString(keyword))
+                );
+
+            Regex number = new Regex("\\d+");
+
+            int maxPages = int.Parse(
+                number.Match(
+                    mainPage.DocumentNode
+                        .SelectNodes("//a")
+                        .Where(x => x.InnerText == "最舊")
+                        .OrderBy(x => x.Attributes["href"].Value.Length).Last()
+                .Attributes["href"].Value).Value ?? ""
+            );
+
+            Random rand = new Random((int)DateTime.Now.Ticks);
+
+            //隨便挑一頁
+            var targetPage = rand.Next(1, maxPages + 1);
+
+            async Task<string> GetRandomImgurUrl(string url) {
+                var tempPage = new HtmlDocument();
+
+                tempPage.LoadHtml(
+                    await client.GetStringAsync(url)
+                );
+
+
+                Regex post = new Regex(@"/bbs/Beauty/M\..+\.html");
+
+                var posts = tempPage.DocumentNode
+                        .SelectNodes("//a")
+                        .Where(x => post.IsMatch(x.Attributes["href"]?.Value ?? ""))
+                        .Select(x => x.Attributes["href"].Value)
+                        .ToArray();
+
+                tempPage.LoadHtml(
+                    await client.GetStringAsync("https://www.ptt.cc" + posts[rand.Next(0, posts.Length)])
+                );
+
+                var pushs = tempPage.DocumentNode.SelectNodes("//div[contains(@class, 'push')]");
+                foreach (var push in pushs) {
+                    push.Remove();
+                }
+
+                Regex imgur = new Regex(@"https?:\/\/i\.imgur\.com\/.*");
+
+                var urls = tempPage.DocumentNode
+                        .SelectNodes("//a")
+                        .Where(x => imgur.IsMatch(x.Attributes["href"]?.Value ?? ""))
+                        .Select(x => x.Attributes["href"].Value)
+                        .ToArray();
+
+                return urls[rand.Next(0, urls.Length)];
+            }
+
+            try {
+                var result = await GetRandomImgurUrl($"https://www.ptt.cc/bbs/Beauty/search?page={targetPage}&q=" + Uri.EscapeDataString(keyword));
+
+                return result.Replace("http://", "https://");
+            } catch {
+                return null;
+            }
+        }
+
     }
 }
